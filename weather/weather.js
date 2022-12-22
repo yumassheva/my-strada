bd0534ed9916c30b63cd0dc2fa8d4ab060e154de
yumassheva <mariia.yumasheva@yandex.ru>
2022-12-22 21:30:41 +0300

    Await,Set()

--- weather/weather.js
+++ weather/weather.js
@@ -1,88 +1,66 @@
 let form = document.forms.first;
-let button = document.querySelector(".but");
 let input = document.querySelector(".input");
-let namecityadd = document.querySelector(".name");
-let temperature = document.querySelector(".grad");
-let rain = document.querySelector(".rain");
-let all = document.querySelector(".all");
-let additional = document.querySelector(".additional");
+let new_favorite_city = document.querySelector(".name");
+let temperature_now = document.querySelector(".grad");
+let icon_weather = document.querySelector(".rain");
+let details_menu = document.querySelector(".additional");
 let like = document.querySelector(".snow");
-let listcity = document.querySelector(".listcity");
-let nameDetails = document.querySelector(".name-details");
+let added_location = document.querySelector(".listcity");
+let city_name_details_menu = document.querySelector(".name-details");
 
-form.addEventListener("submit", function (event) {
-    renderDetails();
-    fetchRequest(input.value);
-    event.preventDefault();
-           
-})
 const storage = {
-    saveCityFavoriteList: function (cityFavoriteList) {
-        
-        localStorage.setItem('cityFavoriteList', JSON.stringify(cityFavoriteList));
+    saveCityFavoriteList: function (set) {
+        localStorage.setItem('cityFavoriteList', JSON.stringify(Array.from(set)));
     },
-    getCityFavoriteList () {
+    getCityFavoriteList() {
         return JSON.parse(localStorage.getItem('cityFavoriteList'));
     },
     getCurrentCity() {
-       return localStorage.getItem('city');
+        return localStorage.getItem('city');
     },
     saveCurrentCity(currentCityName) {
         return localStorage.setItem('city', currentCityName);
     }
 }
-let cityFavoriteList = [
-  //{ name: ''},
-    //{ name: 'Poland' },
-];
 
-function addCityFavorite(newCityFavorite) {
-    let findName = cityFavoriteList.findIndex(function (findName) {
-        return findName.name === newCityFavorite;
-    });
-    if (findName >= 0) {
-        alert('Такой город уже есть');
-        return;
-    } else if (newCityFavorite === "") {
-        alert('Введите название города');
-    } else {
-        cityFavoriteList.push({
-            name: newCityFavorite,
-        });
-    }
-}
-cityFavoriteList = storage.getCityFavoriteList();
-//storage.saveCityFavoriteList(cityFavoriteList);
-//storage.getCityFavoriteList();
+form.addEventListener("submit", function (event) {
+    renderDetails();
+    fetchRequest(input.value);
+    event.preventDefault();
+})
 
+let cityFavoriteList = storage.getCityFavoriteList();
+let set = new Set(cityFavoriteList);
+storage.saveCityFavoriteList(set);  
 fetchRequest(storage.getCurrentCity());
 addUI();
 
 like.addEventListener("click", function () {
     renderNow();
-    addCityFavorite(namecityadd.textContent);
-    storage.saveCityFavoriteList(cityFavoriteList);
+    addCityFavorite(new_favorite_city.textContent);
+    storage.saveCityFavoriteList(set);
     addUI();
 })
+function addCityFavorite(newCityFavorite) {
+    set.add(newCityFavorite);
+}
 function renderDetails() {
-    additional.textContent = "";
+    details_menu.textContent = "";
 }
 function renderNow() {
-   let taski = document.querySelectorAll(".blockcity");
-    for (let tas of taski) {
-        tas.remove();
+    let block_of_city = document.querySelectorAll(".blockcity");
+    for (let city_name of block_of_city) {
+        city_name.remove();
     }
 }
 function addUI() {
     for (i = 0; i < storage.getCityFavoriteList().length; i++) {
-        absol=(storage.getCityFavoriteList()[i].name);
-
         let blockcity = document.createElement("div");
         blockcity.className = "blockcity";
-        listcity.append(blockcity);
+        added_location.append(blockcity);
 
         let newcity = document.createElement("li");
-        newcity.textContent = absol;
+        newcity.textContent = storage.getCityFavoriteList()[i];
         newcity.className = "newcity";
         blockcity.append(newcity);
 
@@ -92,9 +70,8 @@ function addUI() {
         newcity.after(delete_button);
 
         delete_button.addEventListener('click', function deleteTask() {
-            let task = cityFavoriteList.filter(item => item.name !== newcity.textContent);//без мутаций. исходник по ссылке остается
-            cityFavoriteList = task;
-            storage.saveCityFavoriteList(cityFavoriteList);
+            set.delete( newcity.textContent);
+            storage.saveCityFavoriteList(set);
             blockcity.textContent = "";
             blockcity.remove();
         })
@@ -105,26 +82,26 @@ function addUI() {
         })
     }
 }
-function fetchRequest(cityName) {
+async function fetchRequest(cityName) {
     const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
     const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
     const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
-    fetch(url)
-        .then(response => response.json())
-        .then(function (commits) {
-            namecityadd.textContent = commits.name;
-            temperature.textContent = Math.round(commits.main.temp - 273) + '°C';
-            rain.src = `http://openweathermap.org/img/wn/${commits.weather[0].icon}@4x.png`;
+    let response = await fetch(url);
+    let commits = await response.json();
+    
+            new_favorite_city.textContent = commits.name;
+            temperature_now.textContent = Math.round(commits.main.temp - 273) + '°C';
+            icon_weather.src = `http://openweathermap.org/img/wn/${commits.weather[0].icon}@4x.png`;
 
-            nameDetails.textContent = commits.name;
+            city_name_details_menu.textContent = commits.name;
 
-            let tempa = document.createElement("li");
-            tempa.textContent = `Temp:${Math.round(commits.main.temp - 273.13)}°C`;
-            additional.append(tempa);
+            let temperature_details = document.createElement("li");
+            temperature_details.textContent = `Temperature:${Math.round(commits.main.temp - 273.13)}°C`;
+            details_menu.append(temperature_details);
 
             let feels = document.createElement("li");
             feels.textContent = `Feels like:${Math.round(commits.main.feels_like - 273.13)}°C`;
-            tempa.append(feels);
+            temperature_details.append(feels);
 
             let weath = document.createElement("li");
             weath.textContent = `Weather: ${commits.weather[0].main}`;
@@ -139,6 +116,5 @@ function fetchRequest(cityName) {
             let sunse = document.createElement("li");
             sunse.textContent = `Sunset:${time.getHours()}:${t.getMinutes() }`;
             sunris.append(sunse);
-        })
-        .catch(error => alert(error.message));
-}
+        }
+
